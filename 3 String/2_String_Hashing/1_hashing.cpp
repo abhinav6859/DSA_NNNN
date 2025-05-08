dcea9800d4bff40736abd703ad7a30c83bcd1fcb
abhinav6859 <abhinavvats06665@gmail.com>
2025-05-09 00:19:29 +0530

    initial commit

--- 3 String/2_String_Hashing/1_hashing.cpp
+++ 3 String/2_String_Hashing/1_hashing.cpp
@@ -1,3 +1,6 @@
+// Polynomial rolling hash function is a hash function that uses only multiplications and additions. The following is the function:
+// hash(s)
+
 #include<bits/stdc++.h>
 using namespace std;
 #define endl '\n'
@@ -9,9 +12,25 @@ using namespace std;
 typedef vector<int>v;
 typedef vector<string>vs;
 typedef long long int ll;
+typedef vector<ll>vll;
+const int mod=1e9+7;
+const int N=1e5;
+const int p=31;
+vll pp(N+1,1);
+
+ll p_hash (string s){
+    ll ans =0;
+    FOR(i,0,s.length()){
+ans =  ( ans+ (s[i] -'a' +1)*pp[i]) %mod;
+    }
+return  ans;     
+    
+}
 
 void solve(){
-    
+    string s;
+    cin>> s;
+    cout<<p_hash(s) <<endl;
 }
 int main(){
     ios_base::sync_with_stdio(false);
@@ -19,7 +38,8 @@ int main(){
     cout.tie(NULL);
 
     int n;
-     cin >>n;
+    cin >>n;
+     FOR(i,1,N+1) pp[i]=(pp[i-1]*p)%mod;
     while(n--){
   solve();
 }
